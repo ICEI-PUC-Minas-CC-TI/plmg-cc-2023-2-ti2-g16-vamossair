05f362d1183fcf0fc3eecd332dd32820cd292355
Luigi Louback de Oliveira <luigilouback@yahoo.com.br>
2023-09-27 20:04:45 -0300

    carrossel adicionado

--- Codigo/scripts/script.js
+++ Codigo/scripts/script.js
@@ -1,4 +1,5 @@
 // Menu
+
 const btn = document.getElementById('btn-menu');
 const menu = document.getElementById('menu');
 
@@ -156,3 +157,16 @@ function atualizarInterfaceFavoritos(index, id) {
     }
 
 }
+// carrossel
+
+  // JavaScript para controlar o slide automático
+  const slider = document.querySelector('.slider');
+  let currentIndex = 0;
+
+  function nextSlide() {
+      currentIndex = (currentIndex + 1) % slider.children.length;
+      slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }
+
+  // Iniciar o slide automático
+  setInterval(nextSlide, 3000); // Altera o slide a cada 3 segundos
